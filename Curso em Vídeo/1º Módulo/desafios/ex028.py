e1e275488a847911774aae210bdef81b5f40d234
Luca Mateo Rangel <119714632+Rngllz@users.noreply.github.com>
2023-03-27 16:33:28 -0300

    Update ex028.py

--- Curso em Vídeo/1º Módulo/desafios/ex028.py
+++ Curso em Vídeo/1º Módulo/desafios/ex028.py
@@ -1,9 +1,11 @@
 from random import randint
 
-usu_n = int(input('Digite um número de 0 a 5: '))
-
 n = [0,1,2,3,4,5]
 numero = n[randint(0,5)]
+
+usu_n = int(input('Digite um número de 0 a 5: '))
+
+
 
 if usu_n == numero:
     print('Parabêns você acertou o número!')
