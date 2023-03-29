0636107b218c30be6c8896e712a13bce46ab87b3
Luca Mateo Rangel <119714632+Rngllz@users.noreply.github.com>
2023-03-29 18:54:42 -0300

    Create ex055.py

---
+++ Curso em Vídeo/2º Módulo/desafios/ex055.py
@@ -0,0 +1,14 @@
+
+maior = 0
+menor = 0
+
+for c in range(1,6,1):
+    peso = float(input("Peso da {}ª pessoa: ".format(c)))
+    if c == 1:
+        menor = peso
+    if peso > maior:
+        maior = peso
+    if peso < menor:
+        menor = peso
+
+print("\n\033[31mO maior peso lido é de {}.kg\n\033[32mO menor peso lido é de {}kg.\033[m".format(maior,menor))
