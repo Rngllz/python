360008e96731ee5c0c5c8bd579a14bfca553be41
Rngllz <119714632+Rngllz@users.noreply.github.com>
2023-04-05 17:01:21 -0300

    CRÇ 72 - 77

--- Curso em Vídeo/3º Módulo/desafios/ex077.py
+++ Curso em Vídeo/3º Módulo/desafios/ex077.py
@@ -7,3 +7,16 @@ for c in range(0,len(tupla),1):
             print('\033[32m{}'.format(d),end=' ')
     print('\n')
 print('\033[m\n')
+
+# Forma do professor
+print('='*60)
+
+tupla = ('termo','audio','olhar','vitoria','cachorro')
+print('\n')
+for c in tupla:
+    print(f"\033[mA palavra '{c}' possui as seguintes vogais:",end=' ')
+    for d in c:
+        if d in 'AaEeIiOoUu':
+            print('\033[32m{}'.format(d),end=' ')
+    print('\n')
+print('\033[m\n')
