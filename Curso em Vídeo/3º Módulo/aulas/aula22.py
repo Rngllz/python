16eee425c397dad134b78b388e6124d3059f98c6
Rngllz <119714632+Rngllz@users.noreply.github.com>
2023-05-03 17:03:59 -0300

    aula 22

---
+++ Curso em Vídeo/3º Módulo/aulas/aula22.py
@@ -0,0 +1,13 @@
+from aula22pacote import numeros
+
+
+num = int(input("Digite um valor: "))
+fat = numeros.fatorial(num)
+dob = numeros.dobro(num)
+tri = numeros.triplo(num)
+print(f"O fatorial é {fat}.")
+print(f"O dobro é {dob}.")
+print(f"O triplo é {tri}.")
+
+
+
